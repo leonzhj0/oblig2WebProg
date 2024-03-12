21219dd3bdec316ff7a62fce361db973a8d11b12
Leon Zoltan Hjulstad <87582888+leonzhj0@users.noreply.github.com>
2024-03-12 16:59:20 +0100

    weqew

---
+++ src/main/java/oslomet/webprog/Obli2Application.java
@@ -0,0 +1,13 @@
+package oslomet.webprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obli2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Obli2Application.class, args);
+    }
+
+}
