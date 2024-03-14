4c64671132c23de533340949e945d32771b8ba94
Leon Zoltan Hjulstad <87582888+leonzhj0@users.noreply.github.com>
2024-03-15 00:27:05 +0100

    litt javascript må sjekkes på

--- src/main/resources/static/billetter.js
+++ src/main/resources/static/billetter.js
@@ -1,5 +1,4 @@
 $(function(){
-    let billetter = []; //Lager array
     let validering = true;
     $('#tabellBilletter').hide();
 
@@ -8,65 +7,73 @@ $(function(){
 
         //Definerer verdier brukt senere
         let ut;
-        let filmInput = $('#filmer').val();
-        let antallInput = $('#antallBilletter').val();
-        let fornavnInput = $('#fornavn').val();
-        let etternavnInput = $('#etternavn').val();
-        let telefnrInput = $('#telefnr').val();
-        let epostInput = $('#epost').val();
+        const billett = {
+            film: $('#filmer').val(),
+            antall: $('#antallBilletter').val(),
+            fornavn: $('#fornavn').val(),
+            etternavn: $('#etternavn').val(),
+            telefonNr:  $('#telefnr').val(),
+            email: $('#epost').val()
+        };
 
         $('.validering').text(''); //Tømmer div-boksene med feilmeldinger
         validering = true; // Reseter validering
 
         //Sjekker om brukeren har valgt noe annet en "default option"
+        let filmInput = $("#filmer").val();
         if (filmInput == 'fyllTekst'){
             $('#filmValidering').text('Velg en film!');
             validering = false;
         }
         //Sjekker om bruker har fylt inn et tall, større enn null
+        let antallInput = $("#antallBilletter").val();
         if (antallInput == null || antallInput == 0){
             $('#antallValidering').text('Skriv inn et tall større enn null.');
             validering = false;
         }
         let regNavn = /^[a-zA-ZæøåÆØÅ]+$/; //Regex for navn med æøå
+        let fornavnInput = $("#fornavn").val();
         if (regNavn.test(fornavnInput) === false || fornavnInput.trim() === ''){ //tester regex mot input
             $('#fornavnValidering').text('Må skrive inn fornavnet ditt.');
             validering = false;
         }//Trimmer whitespace for å sjekke om input er tom i fornavn og etternavn
+        let etternavnInput = $("#etternavn").val();
         if (regNavn.test(etternavnInput) === false || etternavnInput.trim() === ''){
             $('#etternavnValidering').text('Må skrive inn etternavnet ditt.');
             validering = false;
         }
         //Regex for telefonnr, ulike antall sifre med landskode om bruker ønsker.
         const telefnrRegex = /^(\+\d{1,3})?[-.\s]?\d{1,3}([-.\s]?\d{2,3}){1,3}$/;
+        let telefnrInput = $("#telefnr").val();
         if (!telefnrRegex.test(telefnrInput)) {
             $('#telefnrValidering').text('Skriv inn et gyldig tlfnr.');
             validering = false;
         }
         //regex for epost, tillatter ikke norske bokstaver: æøå.
         const epostRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+        let epostInput = $("#epost").val();
         if (!epostRegex.test(epostInput)){
             $('#epostValidering').text('Skriv inn gyldig epost.');
             validering = false;
         }
         //Om validering er true kjøres dette
         if (validering) {
-            //Lager et objekt som tar inn alle inputs og setter det inn i arrayet definert tidligere
-            const billettKjop = {
-                film: filmInput,
-                antall: antallInput,
-                fornavn: fornavnInput,
-                etternavn: etternavnInput,
-                telefonnr: telefnrInput,
-                epost: epostInput
-            };
-            billetter.push(billettKjop);
-
-            //Definerer utskriften og appender den til html tabellen
-            ut += "<tr><td>" + billettKjop.film + "</td><td>" + billettKjop.antall + "</td><td>" +
-                billettKjop.fornavn + "</td><td>" + billettKjop.etternavn + "</td><td>" + billettKjop.telefonnr
-                + "</td><td>" + billettKjop.epost + "</td></tr>";
-            $('#tabellBilletter').append(ut);
+            $.post("/lagre", billett, function (){
+                hentData();
+            });
+            function hentData(){
+                $.get("/hent", function (data){
+                    formaterOutput(data);
+                })
+            }
+            function formaterOutput(billetter){
+                for(const billett of billetter){
+                    ut += "<tr><td>" + billett.film + "</td><td>" + billett.antall + "</td><td>" +
+                        billett.fornavn + "</td><td>" + billett.etternavn + "</td><td>" + billett.telefonNr
+                        + "</td><td>" + billett.email + "</td></tr>";
+                }
+                $("#tabellRad1").html(ut);
+            }
 
             //Reseter input til slutt slik at kunde kan skrive inn nye verdier
             $('#filmer').val('fyllTekst');
@@ -79,12 +86,8 @@ $(function(){
     });
 
     $('#slettAlle').click(function (){
-        //Tømmer arrayet billetter ved å lage et nytt som heter det samme bare uten noen verdier
-        billetter = [];
-        //Fjerner alle rader inni tbody unntatt den første som er table header
-        $('#tabellBilletter tbody tr:not(:first-child)').empty();
-        //Skjuler tableheader
-        $('#tabellBilletter').hide();
-        //console.log(billetter);
+        $.get("/slettData", function (){
+            hentData();
+        });
     });
 });
